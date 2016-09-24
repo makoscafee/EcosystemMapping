5bfd52c51176cbc77aee69b70f536e758fdfefff
MAKOSCAFEE <barnabasmakonda@gmail.com>
2016-09-25 01:21:25 +0300

    rearranged to easy migrations

--- database/migrations/2016_09_24_210929_create_event_infos_table.php
+++ database/migrations/2016_09_24_210929_create_event_infos_table.php
@@ -16,8 +16,8 @@ class CreateEventInfosTable extends Migration
             $table->increments('id');
             $table->integer('event_id')->unsigned()->nullable()->default(null);
             $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
-            $table->integer('project_id')->unsigned()->nullable()->default(null);
-            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->integer('organization_id')->unsigned()->nullable()->default(null);
+            $table->foreign('organization_id')->references('id')->on('organizations')->onDelete('cascade');
             $table->integer('event_role_id')->unsigned()->nullable()->default(null);
             $table->foreign('event_role_id')->references('id')->on('event_roles')->onDelete('cascade');
             $table->timestamps();
